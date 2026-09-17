db4f19adc1cc677088707b24962a5b3c2d8e57a5
Aadi1052 <aadityakumargupta9999@gmail.com>
2026-09-17 12:03:02 +0530

    feat: update package.json for lab3 with new scripts and dependencies
    feat: add prg4.js to serve product data as JSON
    
    feat: create prg5.js for a simple web server with multiple routes
    
    feat: implement prg6.js to serve HTML files using streams
    
    feat: add product.html for displaying product details
    
    docs: update readme.md with detailed information about HTTP module and status codes
    
    chore: add package-lock.json for lab4 with nodemon as a dev dependency
    
    feat: create package.json for lab4 with scripts for starting and developing
    
    feat: implement prg7.js for a RESTful API with user management
    
    docs: add readme.md for lab4 with setup instructions
    
    feat: create users.js for in-memory user data management

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,30 +1,27 @@
 import http from "http";
 import { getUsers } from "./users.js";
-
 const server = http.createServer((req, res) => {
   if (req.url === "/api/users" && req.method === "GET") {
     res.end(JSON.stringify(getUsers()));
-  } 
-  
-  else if (req.url === "/api/users" && req.method === "POST") {
-    res.end(JSON.stringify({ msg: "add user" }));
-  } 
-  
-  else if (req.url === "/api/users/1" && req.method === "GET") {
+  } else if (req.url === "/api/users" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const user = JSON.parse(body);
+      console.log(user);
+    });
+  } else if (req.url === "/api/users/1" && req.method === "GET") {
     res.end(JSON.stringify({ msg: "single user with id 1" }));
-  } 
-  
-  else if (req.url === "/api/users/1" && req.method === "PUT") {
-    res.end(JSON.stringify({ msg: "update user 1" }));
-  } 
-  
-  else if (req.url === "/api/users/1" && req.method === "DELETE") {
-    res.end(JSON.stringify({ msg: "remove 1" }));
-  } 
-  else {
+  } else if (req.url === "/api/users/1" && req.method === "PUT") {
+    res.end(JSON.stringify({ msg: "update user with id 1" }));
+  } else if (req.url === "/api/users/1" && req.method === "DELETE") {
+    res.end(JSON.stringify({ msg: "remove user with id 1" }));
+  } else {
     res.statusCode = 404;
     res.end();
   }
 });
 
-server.listen(3002, () => console.log("prg7 is running"));
+server.listen(3002, () => console.log("server is running at 3000..."));
